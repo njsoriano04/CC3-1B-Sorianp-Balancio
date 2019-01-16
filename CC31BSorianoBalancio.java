c660456b1e60ce135d2c93aca554cf9c287de2ee
njsoriano04 <44093392+njsoriano04@users.noreply.github.com>
2019-01-16 12:41:32 +0800

    Add files via upload

--- CC31BSorianoBalancio.java
+++ CC31BSorianoBalancio.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cc3.pkg1b.sorianobalancio;
 
 import java.util.Scanner;
